7a525b021873dc13caeaa10e7b2fdaed590b75f3
sirius <370082192@qq.com>
2020-04-18 11:09:46 +0800

    First Commit

---
+++ nsga-ii-master/src/nsga2/OneFunction.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Barnaby Isaac Yves Taylor <github.com/barns>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ */
+package nsga2;
+
+public class OneFunction implements ObjectiveFunction {
+
+	public int calculateObjective(Individual individual) {
+		int leadingOnes = 0;
+		String bitstring = individual.getBitstring();
+
+		for(int i = 0; i < bitstring.length(); i++) {
+		    if (bitstring.charAt(i) == '1') {
+		    	leadingOnes++;
+		    }
+		}
+
+		return leadingOnes;
+	}
+
+	public int getObjectiveValue(Individual individual) {
+		return individual.getLeadingOnes();
+	}
+
+	@Override
+	public int getMostValue(Population population) {
+		return population.getMostOnes();
+	}
+
+	@Override
+	public int getLeastValue(Population population) {
+		return population.getLeastOnes();
+	}
+
+}
---
+++ nsga-ii-master/src/nsga2/ZeroFunction.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Barnaby Isaac Yves Taylor <github.com/barns>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ */
+package nsga2;
+
+public class ZeroFunction implements ObjectiveFunction {
+
+	public int calculateObjective(Individual individual) {
+		int trailingZeros = 0;
+		String bitstring = individual.getBitstring();
+
+		for(int i = 0; i < bitstring.length(); i++) {
+		    if (bitstring.charAt(i) == '0') {
+		    	trailingZeros++;
+		    }
+		}
+
+		return trailingZeros;
+	}
+
+	public int getObjectiveValue(Individual individual) {
+		return individual.getTrailingZeros();
+	}
+
+	@Override
+	public int getMostValue(Population population) {
+		return population.getMostZeros();
+	}
+
+	@Override
+	public int getLeastValue(Population population) {
+		return population.getLeastZeros();
+	}
+
+}
